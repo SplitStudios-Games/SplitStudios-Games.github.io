639c3a888516355b3938ace2e2b24a95644cf895
SplitStudios-Games <splitstudios.games@gmail.com>
2024-01-24 13:52:25 +0200

    google analytics

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+function toggleActive(trigger) {
+    trigger.classList.toggle("active");
+    var lines = document.querySelectorAll('.listRec');
+    var listMenu = document.getElementsByClassName("listMenu")[0];
+
+    if(trigger.classList.contains("active")){
+      lines.forEach(function(element, index){
+        element.style.backgroundColor = "#0099ff";
+      });
+      listMenu.style.display = "flex";
+    }else{
+      lines.forEach(function(element, index){
+        element.style.backgroundColor = "#ffffff";
+      });
+      listMenu.style.display = "none";
+    }
+  }
+
+  function redirectToLink(url) {
+    window.location.href = url;
+  }
