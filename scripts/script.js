f17a4230531b71ea841eeb8dbfc05da94da8c103
SplitStudios <splitstudios.games@gmail.com>
2024-08-10 16:14:40 +0300

    form

--- scripts/script.js
+++ scripts/script.js
@@ -21,34 +21,10 @@ function toggleActive(trigger) {
 
 
   document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const subject = document.getElementById('subject').value;
-    const message = document.getElementById('message').value;
     const responseMessage = document.getElementById('responseMessage');
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://formspree.io/f/xnnavbqa', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            responseMessage.textContent = "Thank you for contacting us!";
-            responseMessage.style.color = "green";
-        } else if (xhr.readyState == 4) {
-            responseMessage.textContent = "Sorry, something went wrong. Please try again.";
-            responseMessage.style.color = "red";
-        }
-    };
-
-    const data = JSON.stringify({
-        name: name,
-        email: email,
-        subject: subject,
-        message: message
-    });
-
-    xhr.send(data);
+    
+    // Show a thank you message on form submission
+    responseMessage.textContent = "Thank you for contacting us!";
+    responseMessage.style.color = "green";
 });
+
